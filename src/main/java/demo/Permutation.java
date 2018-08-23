7f05407c485d9d5be27dc7fe4d5bdf153b306171
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-23 13:56:40 +0800

    [Jessie & Amelia]complete the second implementation when chars is ab

--- src/main/java/demo/Permutation.java
+++ src/main/java/demo/Permutation.java
@@ -1,11 +1,18 @@
 package demo;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Permutation {
     public static List<String> calPermutations(String input) {
+        if(input.length()==2){
+            List<String> permutations  = new ArrayList<>();
+            permutations.add(input);
+            permutations.add(input.substring(1,2)+input.substring(0,1));
+            return permutations;
+        }
         return Arrays.asList(input);
     }
 }
