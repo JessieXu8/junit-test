59f206fe96844bff1b67f70ddac088dc4ec14ded
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-23 14:29:49 +0800

    [Jessie & Amelia]third refactor the third test when chars is abc

--- src/main/java/demo/Permutation.java
+++ src/main/java/demo/Permutation.java
@@ -3,33 +3,25 @@ package demo;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Permutation {
-    public static List<String> calPermutations(String input) {
-        if(input.length()==2){
-            List<String> permutations  = new ArrayList<>();
-            permutations.add(input);
-            permutations.add(input.substring(1,2)+input.substring(0,1));
-            return permutations;
+class Permutation {
+    static List<String> calPermutations(String input) {
+        if (input.length() == 1) {
+            return Collections.singletonList(input);
         }
-        if(input.length()==3){
-            List<String> permutations  = new ArrayList<>();
-            List<String> tempPermutations = calPermutations(removeOne(input,0));
-            permutations.add(input.charAt(0)+tempPermutations.get(0));
-            permutations.add(input.charAt(0)+tempPermutations.get(1));
-            tempPermutations = calPermutations(removeOne(input,1));
-            permutations.add(input.charAt(1)+tempPermutations.get(0));
-            permutations.add(input.charAt(1)+tempPermutations.get(1));
-            tempPermutations = calPermutations(removeOne(input,2));
-            permutations.add(input.charAt(2)+tempPermutations.get(0));
-            permutations.add(input.charAt(2)+tempPermutations.get(1));
-            return permutations;
+        List<String> permutations = new ArrayList<>();
+        for (int index = 0; index < input.length(); index++) {
+            List<String> tempPermutations = calPermutations(removeOne(input, index));
+            for (String tempPermutation : tempPermutations) {
+                permutations.add(input.charAt(index) + tempPermutation);
+            }
         }
-        return Arrays.asList(input);
+        return permutations;
     }
 
-    private static String removeOne(String input,int index) {
-        return input.substring(0,index)+input.substring(index+1);
+    private static String removeOne(String input, int index) {
+        return input.substring(0, index) + input.substring(index + 1);
     }
 }
