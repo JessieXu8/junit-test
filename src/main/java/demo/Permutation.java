aa379211beedff58f139b8707791986d3f719b5f
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-23 14:09:22 +0800

    [Jessie & Amelia]first refactor the third test when chars is abc

--- src/main/java/demo/Permutation.java
+++ src/main/java/demo/Permutation.java
@@ -15,12 +15,15 @@ public class Permutation {
         }
         if(input.length()==3){
             List<String> permutations  = new ArrayList<>();
-            permutations.add(input.substring(0,1)+input.substring(1,2)+input.substring(2,3));
-            permutations.add(input.substring(0,1)+input.substring(2,3)+input.substring(1,2));
-            permutations.add(input.substring(1,2)+input.substring(0,1)+input.substring(2,3));
-            permutations.add(input.substring(1,2)+input.substring(2,3)+input.substring(0,1));
-            permutations.add(input.substring(2,3)+input.substring(0,1)+input.substring(1,2));
-            permutations.add(input.substring(2,3)+input.substring(1,2)+input.substring(0,1));
+            List<String> tempPermutations = calPermutations(input.substring(1,3));
+            permutations.add(input.substring(0,1)+tempPermutations.get(0));
+            permutations.add(input.substring(0,1)+tempPermutations.get(1));
+            tempPermutations = calPermutations(input.substring(0,1)+input.substring(2,3));
+            permutations.add(input.substring(1,2)+tempPermutations.get(0));
+            permutations.add(input.substring(1,2)+tempPermutations.get(1));
+            tempPermutations = calPermutations(input.substring(0,2));
+            permutations.add(input.substring(2,3)+tempPermutations.get(0));
+            permutations.add(input.substring(2,3)+tempPermutations.get(1));
             return permutations;
         }
         return Arrays.asList(input);
